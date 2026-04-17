f51be7ccc3fc401afeb7d83f5e6639d3d93c31be
Sergio Puentes <sergioandrespuentescicery@gmail.com>
2026-04-16 20:51:56 -0500

    Inicializando el proyecto MusicPlay

---
+++ back-end/musicplay/src/main/java/com/musicplay/musicplay/MusicplayApplication.java
@@ -0,0 +1,13 @@
+package com.musicplay.musicplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicplayApplication.class, args);
+	}
+
+}
---
+++ back-end/musicplay/src/test/java/com/musicplay/musicplay/MusicplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicplay.musicplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
